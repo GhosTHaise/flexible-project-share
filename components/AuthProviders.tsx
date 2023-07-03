554f4f40c91ac46fecb17f90cba3c79ba2df0d59
GhosTHaise <Ghostrex2@gmail.com>
2023-07-03 20:39:36 +0300

    Init type PRovider

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,6 +1,22 @@
-import React from 'react'
+"use client"
+
+import { getProviders, signIn} from "next-auth/react";
+import { useState,useEffect } from "react";
+
+type Provider = {
+  id : string,
+  name : string,
+  type : string,
+  signinUrl : string,
+  callbackUrl : string,
+  signinUrlParams? : Record<string,string> | null
+}
+
+type Providers = Record<string,Provider>;
 
 const AuthProviders = () => {
+  const [providers, setProviders] = useState<Providers | null>(null);
+
   return (
     <div>
         AuthProviders
