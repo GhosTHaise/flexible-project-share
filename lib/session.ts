aa2ddd8ea18bfd084f92fbe25ab293364b14e1c1
GhosTHaise <Ghostrex2@gmail.com>
2023-07-06 10:40:42 +0300

    Add NextAuthOptions

--- lib/session.ts
+++ lib/session.ts
@@ -4,3 +4,33 @@ import { AdapterUser } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google"
 import jsonwebtoken from "jsonwebtoken"
 import { JWT } from "next-auth/jwt"
+import { signIn } from "next-auth/react";
+
+export const authOptions : NextAuthOptions = {
+    providers : [
+        GoogleProvider({
+            clientId : "",
+            clientSecret : ""
+        })
+    ],
+    jwt : {
+        encode : ({secret,token}) => {
+
+        },
+        decode : async({secret,token}) => {
+
+        }
+    },
+    theme : {
+        colorScheme : "light",
+        logo : "/logo.png"
+    },
+    callbacks : {
+        async session({ session }){
+
+        },
+        async signIn({ user }){
+            
+        }
+    }
+}
