be7cfff5cd87e9b63d6a827cd25898fbca4b8fea
GhosTHaise <Ghostrex2@gmail.com>
2023-07-06 10:25:21 +0300

    Init provider

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -17,11 +17,29 @@ type Providers = Record<string,Provider>;
 const AuthProviders = () => {
   const [providers, setProviders] = useState<Providers | null>(null);
 
-  return (
-    <div>
-        AuthProviders
-    </div>
-  )
+  useEffect(()=>{
+    const fetchProviders = async () => {
+      const res = await getProviders();
+
+      console.log(res);
+      
+      setProviders(res);
+    }
+    fetchProviders();
+  },[])
+  if(providers){
+    return(
+      <div>
+        {
+          Object.values(providers).map((provider : Provider,i) => (
+            <button key={i}>
+                {provider.id}
+            </button>
+          ))
+        }
+      </div>
+    )
+  }
 }
 
 export default AuthProviders
