a8b758c51fc1270080b880ac89d8e1d64b7e5262
GhosTHaise <Ghostrex2@gmail.com>
2023-07-12 18:44:18 +0300

    Create profile Menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,8 @@ import Image from "next/image"
 import { NavLinks } from "@/constants"
 import AuthProviders from "./AuthProviders";
 import { getCurrentUser } from "@/lib/session";
+import { signOut } from "next-auth/react";
+import ProfileMenu from "./ProfileMenu";
 const Navbar = async () => {
   const session = await getCurrentUser();
   console.log(session);
@@ -35,21 +37,12 @@ const Navbar = async () => {
               {
                 session?.user? (
                   <>
-                    {
-                      session?.user?.image 
-                      &&
-                      <Image 
-                        src={session.user.image}
-                        width={40}
-                        height={40}
-                        className="rounded-full"
-                        alt={session.user.name}
-                      />
-                    }
+                    <ProfileMenu session={session}/>
 
                     <Link href="/create">
                         Share Work
                     </Link>
+                   
                   </>
                 ) : 
                 (
