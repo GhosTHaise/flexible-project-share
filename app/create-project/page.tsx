7d184556fd8b3bf2b82a7ee655322e313842d370
GhosTHaise <Ghostrex2@gmail.com>
2023-07-15 21:36:09 +0300

    Create Modals

--- app/create-project/page.tsx
+++ app/create-project/page.tsx
@@ -1,9 +1,16 @@
+import Modal from "@/components/Modal"
+import ProjectForm from "@/components/ProjectForm"
 
 const CreateProject = () => {
   return (
-    <div>
-            Create-Project
-    </div>
+    <Modal
+
+    >
+            <h3 className="modal-head-text">
+                   Create a New Project     
+            </h3>
+            <ProjectForm />
+    </Modal>
   )
 }
 
---
+++ components/Modal.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useCallback, useRef , ReactNode } from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+
+const Modal = ({children} : {children : ReactNode}) => {
+  return (
+    <div>
+        <div>
+            {children}
+        </div>
+    </div>
+  )
+}
+
+export default Modal
