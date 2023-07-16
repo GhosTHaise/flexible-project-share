251eb238e607761593ed6f27af79a3f94429d8bf
GhosTHaise <Ghostrex2@gmail.com>
2023-07-16 23:16:15 +0300

    Create Form image input

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,4 +1,7 @@
+"use client"
 import { SessionInterface } from "@/common.types"
+import { create } from "domain";
+import { ChangeEvent } from "react";
 
 type Props = {
   type : String,
@@ -6,13 +9,32 @@ type Props = {
 };
 
 const ProjectForm = ({type,session} : Props) => {
+
   const handleSubmit = (e : React.FormEvent) => {};
+  const handleChangeImage = (e : ChangeEvent<HTMLInputElement>) => {};
+
+  const image = null;
   return (
     <form
       onSubmit={handleSubmit}
       className="flexStart form"
     >
-
+        <div className="flexStart form_image-container">
+          <label 
+            htmlFor="image"
+            className="flexCenter form_image-label"
+          >
+            {!image && "Choose a poster for your project"}
+          </label>
+          <input 
+            type="file" 
+            id="image"
+            accept="image/*"
+            required={type === 'create'}
+            className="handleChangeImage"
+            onChange={handleChangeImage}
+          />
+        </div>
     </form>
   )
 }
