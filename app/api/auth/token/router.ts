c7c37b45b452056bc0eafec02e0a8846ab7c358c
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-07-23 10:46:19 +0300

    catch token

---
+++ app/api/auth/token/router.ts
@@ -0,0 +1,23 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse , NextRequest } from "next/server";
+
+const secret = process.env.NEXTAUTH_SECRET;
+
+export async function GET(req : NextRequest){
+    console.log("Req => ", req);
+    
+    const token = await getToken({
+        req,
+        secret,
+        raw : true
+    });
+
+    return NextResponse.json(
+        {
+            token
+        },
+        {
+            status : 200
+        }
+    );
+}
