3cd242d8749e6f50506e46e4e5dcc1d31d8d8490
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-07-25 20:33:54 +0300

    Show user profile avatar

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -30,7 +30,26 @@ const ProjectCard = ({
                     height={314}
                     className='w-full h-full object-cover rounded-2xl'
                 />
+
+                <div className='hidden group-hover:flex profile_card-title'>
+                    <p className='w-full'>
+                        {title}
+                    </p>
+                </div>
         </Link>
+        <div className='flexBetween w-full px-2 mt-3 font-semibold text-sm'>
+            <Link href={`/profile/${userId}`}>
+                <div className='flexCenter gap-2'>
+                    <Image
+                        src={avatarUrl}
+                        alt='Profile Image'
+                        width={24}
+                        height={24}
+                        className='rounded-full'
+                    />
+                </div>
+            </Link>
+        </div>
     </div>
   )
 }
