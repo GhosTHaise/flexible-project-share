021844a9b6eedb254f242ffcb18d895f19f06384
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-07-30 21:09:47 +0300

    List all categories

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -4,8 +4,37 @@ import { usePathname,useRouter,useSearchParams } from "next/navigation"
 import { categoryFilters } from "@/constants"
 
 const Categories = () => {
-  return (
-    <div>Categories 2</div>
+    const router = useRouter();
+    const pathName = usePathname();
+    const searchParams = useSearchParams();
+
+    const category = searchParams.get("category");
+    const handleTags = (filter : string) => {
+
+    }
+    return (
+    <div 
+        className="flexBetween w-full gap-5 flex-wrap"
+    >
+        <ul
+            className="flex gap-2 overflow-auto"
+        >
+            {
+                categoryFilters.map((filter)=>(
+                    <button
+                        key={filter}
+                        type="button"
+                        onClick={() => handleTags(filter)}
+                        className={`${category === filter ? "bg-white font-medium" : "font-normal" }
+                            px-4 py-3 rounded-lg capitalize whitespace-nowrap
+                        `}
+                    >
+                        {filter  }
+                    </button>
+                ))
+            }
+        </ul>
+    </div>
   )
 }
 
